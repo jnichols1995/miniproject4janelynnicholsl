86f020ffcdf285c8388754358a5914a500708335
jnichols1995 <janelyn.nichols12@gmail.com>
2024-04-03 11:27:27 -0500

    Corrected typo--stopping point for lunch

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
-from from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")django.shortcuts import render
+    return HttpResponse("Hello, world. You're at the polls index.")
 
