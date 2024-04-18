e6b663c76412fa79c79040bee1ad776fee106931
jnichols1995 <janelyn.nichols12@gmail.com>
2024-04-18 15:40:18 -0500

    Updates to admin and models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,18 +3,16 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
 
-class Question(models.Model):
-    # ...
     def __str__(self):
         return self.question_text
 
-
- def was_published_recently(self):
+    def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
@@ -23,9 +21,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
 
-
-
-class Choice(models.Model):
-    # ...
     def __str__(self):
         return self.choice_text
